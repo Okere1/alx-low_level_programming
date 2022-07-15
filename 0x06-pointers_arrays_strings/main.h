841eb3b8def3cbc2169b9360107c25840e1895c9
Okere1 <promiseokere958@gmail.com>
2022-07-14 19:38:00 -0700

    made changes onthe main.h file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,7 @@ char *leet(char *n);
 char *rot13(char *n);
 void print_number(int n);
 int main(void);
+char *rot13(char *s);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
