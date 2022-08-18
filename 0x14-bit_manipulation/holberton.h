c9af61671bc982ad06817bf9e2503da49a5d3a26
Promise <87435071+Okere1@users.noreply.github.com>
2022-08-18 17:08:57 +0100

    Code changes

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,11 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
- * File: holberton.h
- * AUth: Brennan D Baraban
+ * File: main.h
+ * AUth: Okere Promise
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
@@ -17,4 +17,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* HOLBERTON_H */
+#endif 
