1d17fd69eedb95558efbad99917afcdd43a66eaa
Promise <87435071+Okere1@users.noreply.github.com>
2022-08-18 17:18:35 +0100

    Code Update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,9 @@
 /*
  * File: 5-flip_bits.c
- * Auth: Brennan D Baraban
+ * Auth: Okere Promise
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * flip_bits - Counts the number of bits needed to be
